5a8b63f973aed5b9eaed4bc3ab4becec3ab1fc2a
magnolia.gcloud <magnolia.gcloud@gmail.com>
2025-11-21 09:15:34 +0000

    feat(dojosphere): show validation rules during entry a character

--- src/app/feature/register/components/register/register.ts
+++ src/app/feature/register/components/register/register.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
+
 import { PanelModule } from 'primeng/panel';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { IftaLabelModule } from 'primeng/iftalabel';
-import { FormsModule } from '@angular/forms';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
+import { MessageModule } from 'primeng/message';
 
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
@@ -14,6 +16,7 @@ import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
   imports: [
     DividerModule,
     PanelModule,
@@ -24,11 +27,56 @@ import { TranslatePipe } from '@ngx-translate/core';
     ButtonModule,
     InputGroupModule,
     InputGroupAddonModule,
+    MessageModule,
     TranslatePipe,
-    FormsModule
+    ReactiveFormsModule
   ],
   templateUrl: './register.html'
 })
 export class Register {
-  value: string | undefined;
+  registerForm: FormGroup;
+  isFormLoading = false;
+  formSubmitted: boolean = false;
+  minPwdLength = 8;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      mail: new FormControl('', [
+        Validators.required, 
+        Validators.email]),
+      pwd: new FormControl('', [
+        Validators.required, 
+        Validators.minLength(this.minPwdLength)])
+    });
+  }
+
+  onSubmit() {
+    this.isFormLoading = true;
+    this.formSubmitted = true;
+    this.isFormLoading = false;
+  }
+
+  isInvalid(controlName: string) {
+    const control = this.registerForm?.get(controlName);
+    return control?.invalid && (control.touched || this.formSubmitted);
+  }
+
+  getPasswordRules() {
+    const pwd: string = this.pwd?.value ?? '';
+  
+    return {
+      hasUpperCase: /[A-Z]/.test(pwd),
+      hasNumber: /[0-9]/.test(pwd),
+      hasSpecialChar: /[^A-Za-z0-9]/.test(pwd),
+      hasMinLength: pwd.length >= this.minPwdLength
+    };
+  }
+
+  get mail() {
+    return this.registerForm.get('mail');
+  }
+
+  get pwd() {
+    return this.registerForm.get('pwd');
+  }
 }
