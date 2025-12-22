e51a9d64e0a221969ade684c9895de87c547ebda
Adrian Helmchen <adrian.helmchen.development@proton.me>
2025-12-22 10:50:04 +0100

    refactor(dojosphere): refactor linting

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+// Reason: The root element is guaranteed by index.html (Vite entry point)
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
