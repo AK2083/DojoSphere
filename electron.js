a0f6e1e81ef39f3eb3e3e18a4936e5d36395ad14
Adrian Helmchen <adrian.helmchen.development@proton.me>
2025-12-30 07:22:35 +0100

    feat(dojosphere): add electron

---
+++ electron.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      devTools: !app.isPackaged,
+    },
+  });
+
+  if (!app.isPackaged) {
+    mainWindow.loadURL('http://localhost:5173');
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+  }
+
+  mainWindow.webContents.on('before-input-event', (event, input) => {
+    if (
+      app.isPackaged &&
+      (input.control || input.meta) &&
+      input.shift &&
+      input.key.toLowerCase() === 'i'
+    ) {
+      event.preventDefault();
+    }
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
