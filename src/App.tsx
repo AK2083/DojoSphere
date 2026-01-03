4e76024bb5df744403960b9f2859b1fa8048b236
Adrian Helmchen <adrian.helmchen.development@proton.me>
2026-01-03 10:33:31 +0100

    feat(dojosphere): linting

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Test GPG</p>
+      <p className="read-the-docs">Test GPG and so on</p>
       <Button variant="contained">Hello world</Button>;
     </>
   );
