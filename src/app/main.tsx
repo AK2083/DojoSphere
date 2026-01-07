cad749c346ae178a273958f5a061d6783ea5837a
Adrian Helmchen <adrian.helmchen.development@proton.me>
2026-01-07 10:25:38 +0100

    feat: add register form layout

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,12 +1,24 @@
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import "./index.css";
 import App from "./App.tsx";
 
 // Reason: The root element is guaranteed by index.html (Vite entry point)
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ThemeProvider
+    theme={createTheme({
+      palette: {
+        mode: "dark",
+      },
+    })}
+  >
+    <CssBaseline />
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ThemeProvider>,
 );
