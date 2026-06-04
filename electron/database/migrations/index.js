f33b54dfb98a1009d392b08fc0e689bda646d791
Adrian Helmchen <adrian.helmchen.development@proton.me>
2026-06-04 16:23:15 +0200

    feat: add permissions to db

--- electron/database/migrations/index.js
+++ electron/database/migrations/index.js
@@ -5,5 +5,9 @@ module.exports = [
   {
     id: '001_create_roles',
     sql: fs.readFileSync(path.join(__dirname, '001_create_roles.sql'), 'utf8')
+  },
+  {
+    id: '002_create_permissions',
+    sql: fs.readFileSync(path.join(__dirname, '002_create_permissions.sql'), 'utf8')
   }
 ]
